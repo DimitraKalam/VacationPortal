95b263f21fa81aec7fec257785daf4c12bc14db6
DimitraKalam <demi.kalam@gmail.com>
2021-05-11 09:15:56 +0300

    login

---
+++ php/main_employee.php
@@ -0,0 +1,22 @@
+<?php
+
+// if(isset($_POST['submit']))
+// {
+//     $name = $_POST['email'];
+//     $password = $_POST['password'];
+// }
+// else
+// {
+//     header("Location: login.php");
+// }
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Main Employee</title>
+    <link rel="stylesheet" type="text/css" href="../css/login.css">
+
+</head>
