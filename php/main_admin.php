e3686b922cbd2a31a93bc7f24e2ba682332ef32b
DimitraKalam <demi.kalam@gmail.com>
2021-05-11 23:36:51 +0300

    list with fields for admin and employee

--- php/main_admin.php
+++ php/main_admin.php
@@ -10,7 +10,28 @@ exit;
 <head>
     <title>Administration page</title>
     <link rel="stylesheet" type="text/css" href="../css/login.css">
-
+    <style>
+        table {
+        border-collapse: collapse;
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+        height: 70px;
+        }
+        table, th, td 
+        {
+        border: 1px solid white;
+        color: white;
+        }
+        th {
+        background-color: #4CAF50;
+        color: white;
+        height: 70px;
+        }
+        td {
+        height: 50px;
+        }
+    </style>
 </head>
 <body>
     <div class="admin_page">
@@ -22,6 +43,46 @@ exit;
         <form id='contact-form' action="logout.php">
             <input type="submit" href="logout.php" name='logout_btn' class="logout_btn" value="Logout"> 
         </form>
+    </div>
+    <br>
+    </div>
+    <div>
+    <?php
+        require_once '../includes/db_connection.php';
+        require_once '../includes/functions.php';
+        $sql="SELECT * from users ";
+        $result= mysqli_query($con,$sql) or die(mysqli_error());
+        $rs=mysqli_fetch_array($result);
+        if(!$con)
+       {
+           die('not connected');
+       }
+        $con=  mysqli_query($con, "select * from users");
+       ?>
+        <div class="users_table">
+         <table>
+            <th>User first name</th>
+            <th>User last name</th>
+            <th>User email</th>
+            <th>User type (employee/admin)</th>
+
+            </tr>
+
+        <?php
+
+             while($row=  mysqli_fetch_array($con))
+             {
+                 ?>
+            <tr>
+                <td><?php echo $row['firstname']; ?></td>
+                <td><?php echo $row['lastname']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['user_type'] ;?></td>
+            </tr>
+        <?php
+             }
+             ?>
+             </table>
     </div>
 </body>
 </html>
--- php/main_employee.php
+++ php/main_employee.php
@@ -1,4 +1,101 @@
 <?php
+// // $sql="SELECT * from request_form where user_id = '".$_GET["sequence"]."' ";
+// require_once '../includes/db_connection.php';
+// require_once '../includes/functions.php';
+// $sql="SELECT * from request_form where request_id = '1' ";
+// $rs= mysqli_query($con,$sql) or die(mysqli_error());
+// $result=mysqli_fetch_array($rs);
+
+// echo '<table>
+// <tr>
+// <td>Start</td>
+// <td>end</td>
+// </tr>
+// <tr>
+// <td>'.$result["start_date"].'</td>
+// <td>'.$result["end_date"].'</td>
+// </tr>
+// </table>';
+
+
+///////
+// require_once '../includes/db_connection.php';
+// require_once '../includes/functions.php';
+// $sql="SELECT * from request_form ";
+// $result= mysqli_query($con,$sql) or die(mysqli_error());
+// $rs=mysqli_fetch_array($result);
+
+// if($result)
+// {
+
+//     echo "<table width ='340' align='left'>
+//           <tr color ='#5D9951>";
+//     $i=0;
+    
+//         If(mysql_num_rows($result)>0)
+//         {
+//              //here you fetch the data from the database and print it in the respective columns   
+//             while($i<mysql_num_fields($result))
+//             {    
+//                  echo "<th>".mysql_field_name($result, $i)."</th>";
+//                  $i++;
+//             }
+//             echo "</tr>";
+    
+//             $color=1;
+    
+//             while($rs)
+//             {    
+//                 If ($color==1){
+//                     echo "<tr color='#'#cccccc'>";
+    
+//                     foreach ($rows as $data){
+//                         echo "<td align='center'>".$data. "</td>";
+//                     }
+    
+//                     $color=2;
+//                 }
+//                 $color=1;
+//             }
+//          }else {
+//             echo"no results found";
+//             echo "</table>";}
+
+// }
+// require_once '../includes/db_connection.php';
+// require_once '../includes/functions.php';
+// $sql="SELECT * from request_form ";
+// $result= mysqli_query($con,$sql) or die(mysqli_error());
+// $rs=mysqli_fetch_array($result);
+// if(!$con)
+// {
+//     die('not connected');
+// }
+// $con=  mysqli_query($con, "select * from request_form");
+
+// while($row=  mysqli_fetch_array($con))
+// {
+//     echo '<table>
+//     <tr>
+//     <td>Forename</td>
+//     <td>Surname</td>
+//     </tr>
+//     <tr>
+//     <td>'.$row['request_id'].'</td>
+//     <td>'.$row['start_date'].'</td>
+//     </tr>
+//     </table>';
+//     //  '<tr>
+//     //     <td>'<.$row['request_id'].>'</td>
+//     //     <td>'<.$row['start_date'].>'</td>
+//     //     <td>'<.$row['end_date'].>'</td>
+//     //     <td>'<.$row['reason'].>'</td>
+//     //     <td>'<.$row['req_status'].>'</td>
+//     //     <td>'<.$row['user_id'].>'</td>
+//     //     <td>'<.$row['date_created'].>'</td>
+//     //     </tr>';
+
+// }
 ?>
 
 
@@ -7,6 +104,28 @@
 <head>
     <title>Main Employee</title>
     <link rel="stylesheet" type="text/css" href="../css/login.css">
+    <style>
+        table {
+        border-collapse: collapse;
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+        height: 70px;
+        }
+        table, th, td 
+        {
+        border: 1px solid white;
+        color: white;
+        }
+        th {
+        background-color: #4CAF50;
+        color: white;
+        height: 70px;
+        }
+        td {
+        height: 50px;
+        }
+    </style>
 </head>
 <body>
     <div class="logout">
@@ -18,6 +137,51 @@
         <form id='contact-form' action="request_form.php">
             <input type="submit" name='submit_request' class="submit_request" value="Submit Request"> 
         </form>
+    <br>
+    </div>
+    <div>
+    <?php
+        require_once '../includes/db_connection.php';
+        require_once '../includes/functions.php';
+        $sql="SELECT * from request_form ";
+        $result= mysqli_query($con,$sql) or die(mysqli_error());
+        $rs=mysqli_fetch_array($result);
+        if(!$con)
+       {
+           die('not connected');
+       }
+        $con=  mysqli_query($con, "select * from request_form");
+       ?>
+        <div class="request_table">
+         <table>
+            <th>Request id</th>
+            <th>Start date</th>
+            <th>End date</th>
+            <th>Reason</th>
+            <th>Request status</th>
+            <th>User id</th>
+            <th>Date created</th>
+
+            </tr>
+
+        <?php
+
+             while($row=  mysqli_fetch_array($con))
+             {
+                 ?>
+            <tr>
+                <td><?php echo $row['request_id']; ?></td>
+                <td><?php echo $row['start_date']; ?></td>
+                <td><?php echo $row['end_date']; ?></td>
+                <td><?php echo $row['reason'] ;?></td>
+                <td><?php echo $row['req_status'] ;?></td>
+                <td><?php echo $row['user_id'] ;?></td>
+                <td><?php echo $row['date_created'] ;?></td>
+            </tr>
+        <?php
+             }
+             ?>
+             </table>
     </div>
 </body>
 </html>
