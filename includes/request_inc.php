611996082efe85f95ec044d374599f86e71a42ea
DimitraKalam <demi.kalam@gmail.com>
2021-05-13 13:26:42 +0300

    send email to supervisor

--- includes/request_inc.php
+++ includes/request_inc.php
@@ -1,5 +1,5 @@
 <?php
-
+session_start();   
 if(isset($_POST['submit_btn']))
 {
 
@@ -7,17 +7,30 @@ if(isset($_POST['submit_btn']))
     $end_date = $_POST['end_date'];
     $reason = $_POST['reason'];
 
-    
     require_once 'db_connection.php';
     require_once 'functions.php';
-
-    submit_form($con,$start_date,$end_date,$reason);
-
+    $email = $_POST['email'];
     
-}
-else
-{
-    header("Location: ../php/login.php");
-    exit();
-}
+        if (submit_form($con,$start_date,$end_date,$reason) !== FALSE)
+    {    
+    $to_email = 'vacation.portal.email@gmail.com';
+    $from = $email;
+    $subject = "Submission Form";
+    $body = "Dear supervisor, employee " .$email. "requested for some time off, 
+    starting on " .$start_date. " and ending on " .$end_date. ", 
+    stating the reason: " .$reason. "
+    Click on one of the below links to approve or reject the application:
+    {approve_link} - {reject_link} ";
+    $headers = "From: ". $from;
+    // mail($to_email, $subject, $body, $headers);
+    $email_sent =mail($to_email, $subject, $body, $from);
+    if ($email_sent == true) {
+        echo "Email successfully sent to $to_email...";
+    } else {
+        echo "Email sending failed...";
+    }
+    header("Location: ../php/main_employee.php");
+    die;
+    }
 
+}
